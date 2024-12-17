3535f46206c58cdaf8f4eb2f49ab25c0153a7776
Leandro Fabio <163411332+Leandro-Fabio-Oliveira@users.noreply.github.com>
2024-12-17 00:40:48 -0300

    Update portfolio_app.py

--- portfolio_app.py
+++ portfolio_app.py
@@ -115,7 +115,7 @@ st.markdown("## My Skills", unsafe_allow_html=True)
 
 st.markdown("""
 
-| Coding Skills  | Beginner_____| Novice_______| Intermediate_| Proficient___| Expert_____|
+| Coding Skills  | Beginner_____| Novice_______| Intermediate_| Proficient____| Expert_____|
 |----------------|:-----------|:------------|:------------ |:-----------|:-----------|
 | Python         |🟩🟩🟩🟩🟩|🟩🟩🟩🟩🟩|🟩🟩🟩🟩🟩 |🟩🟩⬛⬛⬛|⬛⬛⬛⬛⬛|
 | Pandas         |🟩🟩🟩🟩🟩|🟩🟩🟩🟩🟩|🟩🟩🟩🟩🟩 |🟩🟩🟩⬛⬛|⬛⬛⬛⬛⬛|
@@ -128,7 +128,7 @@ st.markdown("""
 
 <br>
 
-| Excel  Skills  | Beginner_____| Novice_______| Intermediate_| Proficient___| Expert_____|
+| Excel  Skills  | Beginner_____| Novice_______| Intermediate_| Proficient____| Expert_____|
 |----------------|:-----------|:------------|:------------ |:-----------|:-----------|
 | Excel          |🟩🟩🟩🟩🟩|🟩🟩🟩🟩🟩|🟩🟩🟩🟩🟩 |🟩🟩🟩🟩🟩|🟩🟩🟩⬛⬛|
 | Formulas       |🟩🟩🟩🟩🟩|🟩🟩🟩🟩🟩|🟩🟩🟩🟩🟩 |🟩🟩🟩🟩🟩|🟩🟩🟩⬛⬛|
@@ -137,7 +137,7 @@ st.markdown("""
 
 <br>
 
-| BI Skills              | Beginner_____| Novice_______| Intermediate_| Proficient___| Expert_____|
+| BI Skills              | Beginner_____| Novice_______| Intermediate_| Proficient____| Expert_____|
 |------------------------|:-----------|:------------|:------------ |:-----------|:-----------|
 | Data Visualization     |🟩🟩🟩🟩🟩|🟩🟩🟩🟩🟩|🟩🟩🟩🟩🟩 |⬛⬛⬛⬛⬛|⬛⬛⬛⬛⬛|
 | Data Modeling          |🟩🟩🟩🟩🟩|🟩🟩🟩🟩🟩|🟩🟩🟩⬛⬛ |⬛⬛⬛⬛⬛|⬛⬛⬛⬛⬛|
